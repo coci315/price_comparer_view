05bc848e4ba446da0028bac33c0f2a45f167f988
coci315 <coci315@gmail.com>
2017-06-21 15:14:48 +0800

    add cancleFocus button on index.vue

--- src/components/focus.vue
+++ src/components/focus.vue
@@ -110,7 +110,7 @@ export default {
   box-sizing: border-box;
   padding-top: 80px;
   width: 100%;
-  height: 100%;
+  min-height: 100%;
   background-color: #F9F9F9;
   .btn_add_wrap {
     text-align: center;
--- src/components/index.vue
+++ src/components/index.vue
@@ -14,11 +14,14 @@
               <span class="price_price">{{product.lowestPrice.price | formatPrice}}</span>
               <span>{{product.lowestPrice.date}}</span>
             </span>
+            <button class="btn btn-danger" @click="delFocus(product.productId)">取消关注</button>
           </div>
-          <echarts :xAxisData="product.price.map(item => {return item.date})" :seriesData="product.price.map(item => {return item.price})"></echarts>
+          <div class="echarts_wrap">
+            <echarts :xAxisData="product.price.map(item => {return item.date})" :seriesData="product.price.map(item => {return item.price})"></echarts>
+          </div>
         </li>
       </ul>
-      <div class="info" v-show="products.length === 0">
+      <div class="info" v-show="showInfo">
         <p>关注列表空空如也</p>
         <p>点击
           <span class="btn_add" @click="goFocus">
@@ -46,23 +49,49 @@ export default {
   },
   data () {
     return {
-      products: []
+      products: [],
+      showInfo: false
     }
   },
   components: {
     echarts
   },
   created () {
-    this.$http.get('/api/products').then(response => {
-      const result = response.data
-      if (result.success) {
-        this.products = result.products
-      }
-    })
+    this.getProducts()
   },
   methods: {
     goFocus () {
       this.$router.push('/focus')
+    },
+    getProducts () {
+      this.$http.get('/api/products').then(response => {
+        const result = response.data
+        if (result.success) {
+          this.products = result.products
+          if (this.products.length === 0) {
+            this.showInfo = true
+          } else {
+            this.showInfo = false
+          }
+        }
+      })
+    },
+    delFocus (productId) {
+      this.$confirm('确定要取消关注吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        console.log('取消')
+        this.$http.delete('/api/focus', {
+          data: { productId: productId }
+        }).then(response => {
+          console.log(response)
+          this.getProducts()
+        })
+      }).catch(() => {
+        return
+      })
     }
   }
 }
@@ -73,7 +102,7 @@ export default {
 .index {
   box-sizing: border-box;
   padding-top: 100px;
-  width: 100%;
+  min-width: 800px;
   min-height: 100%;
   background-color: #F9F9F9;
   .info {
@@ -95,15 +124,23 @@ export default {
   }
   ul {
     cursor: default;
+    min-width: 800px;
+    background-color: #F9F9F9;
     li+li {
       margin-top: 20px;
     }
     li {
+      min-width: 800px;
       .text {
+        min-width: 800px;
         margin-bottom: 10px;
+        text-align: center;
         &>* {
           vertical-align: middle;
         }
+      }
+      .echarts {
+        margin: 0 auto;
       }
       .name {
         margin-left: 20px;
@@ -116,6 +153,7 @@ export default {
       }
       .price {
         margin-left: 20px;
+        margin-right: 20px;
         color: #e4393c;
         .price_price {
           margin-right: 10px;
